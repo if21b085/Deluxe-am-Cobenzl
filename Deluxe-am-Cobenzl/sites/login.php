e9ac786a1f36644b26ef0e4b6a5c5c0201e0b168
HelmutSoellinger <if22b012@technikum-wien.at>
2022-11-15 21:45:27 +0100

    baustein 7

--- Deluxe-am-Cobenzl/sites/login.php
+++ Deluxe-am-Cobenzl/sites/login.php
@@ -1,12 +1,42 @@
-
+<?php
+    $errors = [];
+    $login = [];
+    if(isset($_POST['submitted'])) {
+        $pword;
+        $uname;
+        if (!isset($_POST["uname"]) || empty($_POST["uname"])) {
+            $errors[] = "no uname";
+        }else{
+            $uname = $_POST["uname"];
+        }
+        if (!isset($_POST["pword"]) || empty($_POST["pword"])) {
+            $errors[] = "no password";
+        } else {
+            $pword = $_POST["pword"];
+        }
+        if (!empty($_POST["pword"])&&!empty($_POST["uname"])&&$_POST["uname"] == "gast"&&$_POST["pword"]=="gast"){
+            $login[] = "login"; 
+        }
+    }
+?>
     <div style="text-align:center">
         <h1 >LOGIN</h1>
-        <form action="/action_page.php" target=_blank>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?site=login"  method="post">
             <label for="uname">Username:</label>
-            <input type="text" id="uname" placeholder="Username">
+            <input type="text" id="uname" name="uname" placeholder="Username">
+            <?php 
+                    if(in_array("no uname", $errors))
+                        echo '<p class="text-danger">*Kein Benutzername</p>';
+            ?>
             <label for="pword">Password:</label>
-            <input type="password" id="pword" placeholder="Password" method="post"><br><br>
-            <input type="button" onclick="alert('Success!')" value="Login"><br>
+            <input type="password" id="pword" placeholder="Password" name="pword"><br>
+            <?php 
+                    if(in_array("no password", $errors))
+                        echo '<p class="text-danger">*Kein Kennwort</p>';
+                    if(in_array("login", $login))
+                        echo '<p class="text-success">Willkommen Gast!</p>';
+            ?>
+            <input type="submit" name="submitted" value="Login"><br>
             Noch kein Benutzerkonto?<br>
             <a href="sites/register.php">Hier registrieren!</a>
         </form>
--- Deluxe-am-Cobenzl/sites/register.php
+++ Deluxe-am-Cobenzl/sites/register.php
@@ -1,43 +1,110 @@
+<?php
+    $errors = [];
 
+    if(isset($_POST['submitted'])) {
+        $pword;
+        $pword2;
+        if (!isset($_POST["uname"]) || empty($_POST["uname"])) {
+            $errors[] = "no uname";
+        }
+        if (!isset($_POST["email"]) || empty($_POST["email"])) {
+            $errors[] = "no email";
+        }
+        if (!isset($_POST["pword"]) || empty($_POST["pword"])) {
+            $errors[] = "no password";
+        } else {
+            $pword = $_POST["pword"];
+        }
+        if (!isset($_POST["pword2"]) || empty($_POST["pword2"])) {
+                $errors[] = "no password2";
+        } else {
+            $pword2 = $_POST["pword2"];
+        }
+        if(!empty($pword)&&!empty($pword2)&& $pword!=$pword2) {
+            $errors[] = "passwords not equal";
+        }
+        if (!isset($_POST["vname"]) || empty($_POST["vname"])) {
+            $errors[] = "no vname";
+        }
+        if (!isset($_POST["nname"]) || empty($_POST["nname"])) {
+            $errors[] = "no nname";
+        }
+        if (!isset($_POST["agb"]) || empty($_POST["agb"])) {
+            $errors[] = "no agb";
+        }
+    }
+    
+    ?>
     <div  style="text-align:center">
         <h2 >REGISTRIERUNG</h2>
-        <div class="row >
-        <form action="sites/action_page.php" method="post">
+        <div >
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?site=register" method="post">
+            <div class="row">
+                <div class="col-sm-6">
+                    <fieldset>
+                        <legend><strong>Benutzer Daten:</strong></legend>
+                        <label for="uname">*Username:</label>
+                        <input type="text" id="uname" placeholder="Username" name="uname"><br>
+                        <?php 
+                            if(in_array("no uname", $errors))
+                                echo '<p class="text-danger">*Kein Benutzername</p>';
+                        ?>
+                        <label for="pword">*Password:</label>
+                        <input type="password" id="pword" placeholder="Password" name="pword"><br>
+                        <?php 
+                            if(in_array("no password", $errors))
+                                echo '<p class="text-danger">*Kein Kennwort</p>';
+                        ?>
 
-            <div class="col-sm-6">
-            <fieldset>
-                <legend><strong>Benutzer Daten:</strong></legend>
-                <label for="uname">*Username:</label>
-                <input type="text" id="uname" placeholder="Username"><br>
-                <label for="pword">*Password:</label>
-                <input type="password" id="pword" placeholder="Password" method="post"><br>
-                <label for="pword">*Password-Wiederholung:</label>
-                <input type="password" id="pword" placeholder="Password" method="post"><br>
-                <label for="email">*Enter your email:</label>
-                <input type="email" id="email" name="email">
-            </fieldset>
-            </div>
-            <div class="col-sm-6">
-            <fieldset>
-                <legend><strong>Persönliche Daten:</strong></legend>
-                <label for="anrede">*Anrede:</label>
-                <select id="anrede" name="anrede">
-                    <option value="waehlen">bitte wählen</option>
-                    <option value="frau">Frau</option>
-                    <option value="herr">Herr</option>
-                </select><br><br>
+                        <label for="pword2">*Password-Wiederholung:</label>
+                        <input type="password" id="pword2" placeholder="Password" name="pword2"><br>
+                        <?php 
+                            if(in_array("no password2", $errors))
+                                echo '<p class="text-danger">*Keine Kennwortwiederholung</p>';
+                            if(in_array("passwords not equal", $errors))
+                                echo '<p class="text-danger">*Kennwörter nicht gleich!</p>';
+                        ?>
 
-                <label for="vorname">*Vorname:</label>
-                <input type="text" id="vorname" placeholder="Vorname"><br><br>
-
-                <label for="nachname">*Nachname:</label>
-                <input type="text" id="nachname" placeholder="Nachname"><br>
-            </fieldset>
-            </div>
-            <div class="col-12">
-                <input type="checkbox" id="agb" name="agb" value="agb">
-                <label id="agb" for="agb"> Ich bin damit einverstanden, dass diese Daten zur Anmeldung benutzt werden.</label><br>
-                <input type="button" onclick="alert('Success!')" value="anmelden"><br><br><br>
+                        <label for="email">*Enter your email:</label>
+                        <input type="email" id="email" name="email">
+                        <?php 
+                            if(in_array("no email", $errors))
+                                echo '<p class="text-danger">*Keine E-mailadresse</p>';
+                        ?>
+                    </fieldset>
+                </div>
+                <div class="col-sm-6">
+                    <fieldset>
+                        <legend><strong>Persönliche Daten:</strong></legend>
+                        <label for="anrede">Anrede:</label>
+                        <select id="anrede">
+                            <option value="waehlen">bitte wählen</option>
+                            <option value="frau" name="anrede">Frau</option>
+                            <option value="herr" name="anrede">Herr</option>
+                        </select><br><br>
+                        <label for="vorname">*Vorname:</label>
+                        <input type="text" id="vorname" name="vname" placeholder="Vorname"><br>
+                        <?php 
+                            if(in_array("no vname", $errors))
+                                echo '<p class="text-danger">*Kein Vorname</p>';
+                        ?>
+                        <label for="nachname">*Nachname:</label>
+                        <input type="text" id="nachname" name="nname" placeholder="Nachname"><br>
+                        <?php 
+                            if(in_array("no nname", $errors))
+                                echo '<p class="text-danger">*Kein Nachname</p>';
+                        ?>
+                    </fieldset>
+                </div>
+                <div class="col-12">
+                    <input type="checkbox" id="agb" name="agb" value="agb">
+                    <label id="agb" for="agb"> Ich bin damit einverstanden, dass diese Daten zur Anmeldung benutzt werden.</label><br>
+                    <?php 
+                            if(in_array("no agb", $errors))
+                                echo '<p class="text-danger">*AGB muss akzeptiert werden!</p>';
+                        ?>
+                    <input type="submit" name="submitted" value="Anmelden"><br><br><br>
+                </div>
             </div>
         </form>
     </div>
